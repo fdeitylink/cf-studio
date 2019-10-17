69a85166160c1814a52150721d86dd0d19d32521
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-10-17 12:08:02 -0400

    added labels to main settings grid

--- src/kero_edit/edit/settings_grid.clj
+++ src/kero_edit/edit/settings_grid.clj
@@ -1,9 +1,42 @@
 (ns kero-edit.edit.settings-grid
-  (:require [cljfx.api :as fx]))
+  (:require [cljfx.api :as fx]
+            [kero-edit.edit.i18n :refer [sub-translate]])
+  (:import [javafx.scene.text Font FontWeight]))
+
+(def ^:private font (Font/font "" FontWeight/BOLD 15.0))
 
 (defn settings-grid
   [{:keys [fx/context]}]
   {:fx/type :grid-pane
    :padding 10
    :vgap 10
-   :hgap 20})
+   :hgap 20
+   :children [{:fx/type :label
+               :text (fx/sub context sub-translate ::displayed-layers)
+               :font font
+               :grid-pane/row 0
+               :grid-pane/column 0}
+
+              {:fx/type :label
+               :text (fx/sub context sub-translate ::selected-layer)
+               :font font
+               :grid-pane/row 0
+               :grid-pane/column 1}
+
+              {:fx/type :label
+               :text (fx/sub context sub-translate ::draw-mode)
+               :font font
+               :grid-pane/row 0
+               :grid-pane/column 2}
+
+              {:fx/type :label
+               :text (fx/sub context sub-translate ::view-toggles)
+               :font font
+               :grid-pane/row 0
+               :grid-pane/column 3}
+
+              {:fx/type :label
+               :text (fx/sub context sub-translate ::edit-mode)
+               :font font
+               :grid-pane/row 0
+               :grid-pane/column 4}]})
