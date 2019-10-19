03f8f42b78bfecd044c43950935b23703b7987b3
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-10-19 19:26:53 -0400

    Added type hint to event handler

--- src/kero_edit/edit/events.clj
+++ src/kero_edit/edit/events.clj
@@ -1,12 +1,13 @@
 (ns kero-edit.edit.events
   (:require [cljfx.api :as fx])
-  (:import [javafx.scene.control Dialog DialogEvent ButtonType ButtonBar$ButtonData]))
+  (:import [javafx.event Event]
+           [javafx.scene.control Dialog DialogEvent ButtonType ButtonBar$ButtonData]))
 
 (defmulti event-handler ::type)
 
 (defmethod event-handler ::notepad-text-changed [{:keys [fx/event fx/context]}]
   {:context (fx/swap-context context assoc :notepad-text event)})
 
-(defmethod event-handler ::license-dialog-consumed [{:keys [fx/event fx/context]}]
+(defmethod event-handler ::license-dialog-consumed [{:keys [^Event fx/event fx/context]}]
   {:context (fx/swap-context context assoc :license-accepted
                              (= ButtonBar$ButtonData/YES (.getButtonData ^ButtonType (.getResult ^Dialog (.getSource event)))))})
