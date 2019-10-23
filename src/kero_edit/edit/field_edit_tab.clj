2019790f901077dc21c14fba41dfea1be845ff85
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-10-23 15:30:08 -0400

    Added tooltip of field description to field edit tab

--- src/kero_edit/edit/field_edit_tab.clj
+++ src/kero_edit/edit/field_edit_tab.clj
@@ -1,6 +1,8 @@
 (ns kero-edit.edit.field-edit-tab
   (:require [me.raynes.fs :as fs]
             [cljfx.api :as fx]
+            [kero-edit.kero.field.pxpack :as pxpack]
+            [kero-edit.kero.field.head :as head]
             [kero-edit.edit.events :as events]))
 
 (defn field-edit-tab
@@ -9,4 +11,6 @@
     {:fx/type :tab
      :id name
      :text name
+     :tooltip {:fx/type :tooltip
+               :text (get-in field [:data ::pxpack/head ::head/description])}
      :on-closed {::events/type ::events/close-field :field field}}))
