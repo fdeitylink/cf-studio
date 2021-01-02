bcf7272e469f962480d4945a1ec62244c6719288
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2021-01-02 03:00:22 -0500

    Updated clj-commons/fs dependency to 1.6.307

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[aysylu/loom "1.0.2"]
-                 [clj-commons/fs "1.5.2"]
+                 [clj-commons/fs "1.6.307"]
                  [cljfx "1.7.11"]
                  [cljfx/css "1.1.0"]
                  [com.taoensso/tempura "1.2.1"]
